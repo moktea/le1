aeac3e052c939224ecf78ce8dd3ce46aa7c61452
moktea <92356644+moktea@users.noreply.github.com>
2023-12-16 22:41:29 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# le1
+
+Developed with Unreal Engine 5
---
+++ Source/le1/le1GameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "le1GameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LE1_API Ale1GameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
